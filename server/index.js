9949a0f095f85f1be75074e42d2cbb73dbc749a0
Joel <joel.krebs@gmail.com>
2015-12-06 15:28:11 -0800

    minor server changes

--- server/index.js
+++ server/index.js
@@ -17,56 +17,58 @@ app.use(morgan("combined"));
 app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  res.render("users", {users: users.getAll()});
+	res.render("users", {
+		users: users.getAll()
+	});
 });
 
 app.post("/auth", (req, res, next) => {
-  request(
-    getAuthURL(req.body.logintype) + req.body.token,
-    (error, response, body) => {
-      if (error) {
-        console.warn(error);
-        res.status(500).send("Error.");
-        return;
-      }
+	request(
+		getAuthURL(req.body.logintype) + req.body.token, (error, response, body) => {
+			if (error) {
+				console.warn(error);
+				res.status(500).send("Error.");
+				return;
+			}
 
-      if (response.statusCode !== 200) {
-        console.warn(body);
-        res.status(500).send("Error.");
-        return;
-      }
-      handleOAuthReponse(req.body.logintype, res, JSON.parse(body));     
-    }
-  );
+			if (response.statusCode !== 200) {
+				console.warn(body);
+				res.status(500).send("Error.");
+				return;
+			}
+			handleOAuthReponse(req.body.logintype, res, JSON.parse(body));
+		}
+	);
 });
 
 app.use((req, res, next) => {
-  res.status(404).send("Not found.");
+	res.status(404).send("Not found.");
 });
 
 var server = http.createServer(app);
 server.listen(8090);
 
 function handleOAuthReponse(logintype, res, body) {
-  switch(logintype) {
-    case 'Facebook':
-      users.addUser(body);
-      res.status(200).send("Success.");
-      break;
-    case 'Google':
-      if(groups.checkMembership(res, body.email)) {
-        users.addUser(body);
-      }
-      break;
-  }
+	switch (logintype) {
+		case 'Facebook':
+			users.addUser(body);
+			res.status(200).send("Success.");
+			break;
+		case 'Google':
+			if (groups.checkMembership(res, body.email)) {
+				users.addUser(body);
+        res.status(200).send("Success.");
+			}
+			break;
+	}
 }
 
 function getAuthURL(logintype) {
-  switch(logintype) {
-    case 'Facebook':
-      return 'https://graph.facebook.com/v2.5/me?fields=id,name,picture&access_token=';
-    case 'Google':
-      return 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=';
-  }
-  return undefined
+	switch (logintype) {
+		case 'Facebook':
+			return 'https://graph.facebook.com/v2.5/me?fields=id,name,picture&access_token=';
+		case 'Google':
+			return 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=';
+	}
+	return undefined
 }
